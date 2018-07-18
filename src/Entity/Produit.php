daeffa3a29a722e40f42bc744d533944f9900f66
laurentws <laurent@websuit.fr>
2018-07-18 21:04:22 +0200

    init

---
+++ src/Entity/Produit.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ProduitRepository")
+ */
+class Produit
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message="j'veux un jpeg !")
+     * @Assert\File(mimeTypes={ "image/jpeg" })
+     */
+    private $image; // IL FAUT ACTIVER L'EXTENSION FILEINFO DANS LE PHP.INI
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+}
